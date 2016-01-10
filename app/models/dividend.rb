16ad952d70bf966f030486fa84b242d191ab603f
Gary Teh <garyjob@Garys-MacBook-Air.local>
2016-01-10 12:21:45 -0800

    ensures dividend updates

--- app/models/dividend.rb
+++ app/models/dividend.rb
@@ -1,5 +1,15 @@
 class Dividend < ActiveRecord::Base
+  after_validation :set_is_latest
   after_commit :compute_aggregated_dividend
+
+  def set_is_latest
+    latest_date = Dividend.where(stock_id: stock_id).order(issue_date: :desc).limit(1).pluck(:issue_date).first
+    if latest_date <= self.issue_date
+      Dividend.where(stock_id: stock_id, is_latest: false)
+      self.is_latest = true
+    else
+    end
+  end
 
   def compute_aggregated_dividend
     stock = Stock.find stock_id
